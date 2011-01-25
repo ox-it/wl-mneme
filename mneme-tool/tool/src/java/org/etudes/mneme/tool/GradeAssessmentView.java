59ff61d0bf25a3e92d9de2bd2ab2e424583ece42
Marc Savitsky <marc.savitsky@oucs.ox.ac.uk>
2011-01-25 15:45:00 +0000

    WL-1328 Add CSV export of student scores

--- mneme-tool/tool/src/java/org/etudes/mneme/tool/GradeAssessmentView.java
+++ mneme-tool/tool/src/java/org/etudes/mneme/tool/GradeAssessmentView.java
@@ -25,6 +25,8 @@
 package org.etudes.mneme.tool;
 
 import java.io.IOException;
+import java.io.OutputStream;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
@@ -50,6 +52,8 @@ import org.etudes.mneme.api.SubmissionService;
 import org.sakaiproject.component.api.ServerConfigurationService;
 import org.sakaiproject.tool.api.ToolManager;
 import org.sakaiproject.user.api.UserDirectoryService;
+import org.sakaiproject.user.api.User;
+import org.sakaiproject.user.api.UserNotDefinedException;
 import org.sakaiproject.util.StringUtil;
 import org.sakaiproject.util.Web;
 
@@ -78,6 +82,9 @@ public class GradeAssessmentView extends ControllerImpl
 	
 	/** Dependency: UserDirectory Service */
 	protected UserDirectoryService userDirectoryService = null;
+
+	
+	protected SimpleDateFormat sdf = new SimpleDateFormat("MMM dd, yyyy hh:mm aa");
 
 	/**
 	 * Shutdown.
@@ -410,6 +417,54 @@ public class GradeAssessmentView extends ControllerImpl
 		}
 
 		res.sendRedirect(res.encodeRedirectURL(Web.returnUrl(req, destination)));
+		
+		if (destination.equals("EXPORT")) 
+		{
+			String fileName = assessment.getTitle().replaceAll(" ", "_")+".csv";
+			StringBuffer sb = new StringBuffer();
+			sb.append("\"Name\",\"OSS id\",\"Tries\",\"Finished\",\"Auto Score\",\"Final\",\"Evaluated\",\"Released\"\n");
+			
+			Iterator iter = submissions.getSet().iterator();
+			while (iter.hasNext()) {
+				Object object = iter.next();
+				if (object instanceof Submission) {
+					Submission submission = (Submission)object;
+					sb.append(toCSV(submission));
+					sb.append("\n");
+				}
+			}
+			String csvString = sb.toString();
+			
+			res.setContentType("text/comma-separated-values");
+			String disposition = "attachment; fileName="+fileName;
+            res.setHeader("Content-Disposition", disposition);
+			res.setHeader("Cache-Control", "max-age=0");
+			res.setContentLength(csvString.length());
+			OutputStream out = null;
+			try {
+				out = res.getOutputStream();
+				out.write(csvString.getBytes());
+				out.flush();
+				
+			} catch(IOException e) {
+				e.printStackTrace();
+				
+			} finally {
+				
+				try {
+					if(out != null) out.close();
+					
+				} catch(IOException e) {
+					e.printStackTrace();
+				}
+			}
+			
+			destination = context.getDestination();
+			
+		} else {
+			
+			res.sendRedirect(res.encodeRedirectURL(Web.returnUrl(req, destination)));
+		}
 	}
 
 	/**
@@ -549,4 +604,58 @@ public class GradeAssessmentView extends ControllerImpl
 
 		return sort;
 	}
+	
+	private String toCSV(Submission submission) {
+		StringBuffer sb = new StringBuffer();
+		
+		try {
+			User user;
+			user = userDirectoryService.getUser(submission.getUserId());
+			sb.append("\""+user.getLastName()+", "+user.getFirstName()+"\"");
+			sb.append(",");
+			sb.append("\""+user.getDisplayId()+"\"");
+		} catch (UserNotDefinedException e) {
+			sb.append("\"not known\",\"not known\"");
+		}
+			
+		sb.append(",");
+		
+		sb.append("\""+submission.getSiblingCount()+"/");
+		if (null != submission.getAssessment()) {
+			sb.append(submission.getAssessment().getTries()+"\"");
+		} else {
+			sb.append("infinite\"");
+		}
+		sb.append(",");
+		
+		if (null != submission.getSubmittedDate()) {
+			sb.append("\""+sdf.format(submission.getSubmittedDate())+"\"");
+		} else {
+			sb.append("\"not started\"");
+		}
+		
+		sb.append(",");
+		if (null != submission.getAnswersAutoScore()) {
+			sb.append("\""+submission.getAnswersAutoScore()+"\"");
+		}
+		
+		sb.append(",");
+		if (null != submission.getTotalScore()) {
+			sb.append("\""+submission.getTotalScore()+"\"");
+		}
+		
+		sb.append(",");
+		if (null != submission.getEvaluation() && 
+				null != submission.getEvaluation().getEvaluated()) {
+			sb.append("\""+submission.getEvaluation().getEvaluated()+"\"");
+		}
+		
+		sb.append(",");
+		if (null != submission.getIsReleased()) {
+			sb.append("\""+submission.getIsReleased()+"\"");
+		}
+		
+		return sb.toString();
+	}
+	
 }
